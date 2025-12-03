1f37091cb65e5760f6b7bc2b44b6ec8871caaff2
anumethod <jaymagik095@gmail.com>
2025-12-03 10:42:40 -0500

    fix: Upgrade Next.js to 16.0.7 and React to 18.3.1 to resolve critical vulnerabilities, updating build manifests and type definitions.

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-60785faceb2046e0.js","static/chunks/4bd1b696-c023c6e3521b1417.js","static/chunks/255-40634877ae3e8e9d.js","static/chunks/main-app-3da583b834d76c13.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-60785faceb2046e0.js","static/chunks/framework-a6e0b7e30f98059a.js","static/chunks/main-01cf5cb2cf030074.js","static/chunks/pages/_app-7d307437aca18ad4.js"],"/_error":["static/chunks/webpack-60785faceb2046e0.js","static/chunks/framework-a6e0b7e30f98059a.js","static/chunks/main-01cf5cb2cf030074.js","static/chunks/pages/_error-cb2a52f75f2162e2.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/6241cac1bd5b1e91.js",
+    "static/chunks/5a95628de5be6687.js",
+    "static/chunks/6740f161f60c6ab5.js",
+    "static/chunks/9c23f44fff36548a.js",
+    "static/chunks/turbopack-d8ea2cb20f43aa1f.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"p2YtrL/fxnj+RCe1AKFJlsj/CiTrw6gHJO1jTr6Wbyk=\"\n}"
